1c322b269b242af387f406f4ce27cbd97ac005c5
Kyle Hek <107079031+KyleHek@users.noreply.github.com>
2023-04-27 14:17:12 +0100

    Update Ball.js

--- Ball.js
+++ Ball.js
@@ -1,5 +1,5 @@
-const INTITIAL_VELOCITY = .025
-const VELOCITY_INCREASE = .000005
+const INTITIAL_VELOCITY = .03
+const VELOCITY_INCREASE = .000002
 export default class Ball {
     constructor(ballElem) {
         this.ballElem = ballElem
